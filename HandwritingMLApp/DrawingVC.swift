934812df994bccf0daa3e18a9e28ea8370367e69
Victor Yamchinov <kodiers@gmail.com>
2018-06-07 05:11:27 +0300

    added drawing on screen and mlmodel

--- HandwritingMLApp/DrawingVC.swift
+++ HandwritingMLApp/DrawingVC.swift
@@ -13,6 +13,9 @@ class DrawingVC: UIViewController {
     @IBOutlet weak var drawingImageView: UIImageView!
     @IBOutlet weak var predictionLbl: UILabel!
     
+    var lastPoint = CGPoint.zero
+    var swiped = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,6 +24,44 @@ class DrawingVC: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        swiped = false
+        drawingImageView.image = nil
+        if let touch = touches.first {
+            lastPoint = touch.location(in: drawingImageView)
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        swiped = true
+        if let touch = touches.first {
+            let currentPoint = touch.location(in: drawingImageView)
+            drawLine(fromPoint: lastPoint, toPoint: currentPoint)
+            lastPoint = currentPoint
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if swiped == false {
+            drawLine(fromPoint: lastPoint, toPoint: lastPoint)
+        }
+    }
+    
+    func drawLine(fromPoint: CGPoint, toPoint: CGPoint) {
+        UIGraphicsBeginImageContext(drawingImageView.frame.size)
+        let context = UIGraphicsGetCurrentContext()
+        drawingImageView.image?.draw(in: CGRect(x: 0, y: 0, width: drawingImageView.frame.size.width, height: drawingImageView.frame.size.height))
+        context?.move(to: CGPoint(x: fromPoint.x, y: fromPoint.y))
+        context?.addLine(to: CGPoint(x: toPoint.x, y: toPoint.y))
+        context?.setLineCap(.round)
+        context?.setLineWidth(10.0)
+        context?.setStrokeColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0))
+        context?.setBlendMode(.normal)
+        context?.strokePath()
+        drawingImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
     }
 
     @IBAction func predictBtnPressed(_ sender: Any) {
